081e46e1ef6f9cb4f599898774aaf7841a048d61
mikera <mike.silva@pixter.com.br>
2023-10-01 14:03:57 -0300

    test list transactions

--- test/exemple.spec.ts
+++ test/exemple.spec.ts
@@ -1,4 +1,4 @@
-import { it, beforeAll, afterAll, beforeEach } from 'vitest'
+import { it, beforeAll, afterAll, beforeEach, expect } from 'vitest'
 import { execSync } from 'node:child_process'
 import supertest from 'supertest'
 
@@ -26,4 +26,28 @@ it('should be able to create transaction', async () => {
       type: 'credit',
     })
     .expect(201)
+})
+
+it('should be able to list all transactions', async () => {
+  const createTransactionResponse = await supertest(app.server)
+    .post('/transactions')
+    .send({
+      title: 'New transaction',
+      amount: 1000,
+      type: 'credit',
+    })
+
+  const cookies = createTransactionResponse.get('Set-Cookie')
+
+  const listTransactionResponse = await supertest(app.server)
+    .get('/transactions')
+    .set('Cookie', cookies)
+    .expect(200)
+
+  expect(listTransactionResponse.body.transactions).toEqual([
+    expect.objectContaining({
+      title: 'New transaction',
+      amount: 1000,
+    }),
+  ])
 })
